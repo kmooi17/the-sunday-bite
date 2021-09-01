171a70f39e2d81c1504290c158c66dea89fb02a2
Kane Mooi <kane.mooi@proquoai.com>
2021-09-01 18:52:03 +0200

    adding smtp.js and template content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// import './../assets/js/smtp.js';
+// declare let Email: any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'the-sunday-bite';
+  // This works :)
+  // Email.send({
+  //   SecureToken: '117678fd-9112-4534-a795-be49a5c3d223',
+  //   To: 'thesundaybite-9684cd@inbox.mailtrap.io',
+  //   From: 'test@test.com',
+  //   Subject: 'This is the subject',
+  //   Body: 'And this is the body'
+  // }).then((message: any) => alert(message));
+
 }
