ca269ab0c86c11159ad880f4291b70761f00dab6
kane <kane.mooi@proquoai.com>
2022-01-23 14:41:06 +0200

    updating prices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,68 +8,76 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   cookies: Cookies[] = [
     {
-      active: true, image: 'astros', flavour: 'Astros',
+      active: true, image: 'astros', flavour: 'Astronomical Swirl',
       description: `A vanilla soft cookie with the most amount of astros you can imagine, topped off with velvet cream cheese frosting with a twist.`,
-      price: [50, 90], class: 'card-body two'
+      price: [75, 115], class: 'card-body one'
     },
     {
-      active: true, image: 'bday-cake', flavour: 'Birthday Cake Milkshake',
+      active: true, image: 'bday-cake', flavour: 'Confetti Bomb',
       description: `Your childhood dream in a cookie. It tastes just like your mom's cake batter, topped with the best tasting cream cheese frosting.`,
-      price: [45, 85], class: 'card-body one'
+      price: [70, 110], class: 'card-body two'
     },
     {
-      active: true, image: 'candy-floss', flavour: 'Candyfloss',
+      active: true, image: 'candy-floss', flavour: 'Candy Rain',
       description: `Have you ever had a carnival in your mouth? Well now you can! You think it tastes great... wait until you smell it.`,
-      price: [50, 90], class: 'card-body three'
+      price: [70, 110], class: 'card-body three'
     },
     {
-      active: true, image: 'coffee', flavour: 'Cappuccino',
+      active: true, image: 'coffee', flavour: 'Cappa-Cookie',
       description: `Coffee lovers, this one is for you. Have you ever wanted to eat your coffee instead of drink it? Now's your chance.`,
-      price: [45, 85], class: 'card-body one'
+      price: [60, 100], class: 'card-body one'
     },
     {
-      active: true, image: 'pancakes', flavour: 'Cinnamon & Sugar Pancake',
+      active: true, image: 'pancakes', flavour: 'Surgary-Cin',
       description: ` Ever taken a bite from heaven? Here's your chance with the perfect balance between sweet & savoury. It will always have you coming back for more.`,
-      price: [40, 80], class: 'card-body two'
+      price: [60, 100], class: 'card-body two'
     },
     {
-      active: true, image: 'mint', flavour: 'Choc Mint',
+      active: true, image: 'mint', flavour: 'Mental Mint Bombs',
       description: `Love chocolate but find it too sweet? This is the perfect balance. Chocolate & Mint - name a better duo.`,
-      price: [40, 80], class: 'card-body three'
+      price: [60, 100], class: 'card-body three'
     },
     {
-      active: true, image: 'choc', flavour: 'Death by Chocolate',
+      active: true, image: 'choc', flavour: 'Chocolate Typhoon',
       description: `Attention all choco-holics! Chocolate three ways, sounds like a party at your place. A bite to die for worth every moment on the hips.`,
-      price: [45, 85], class: 'card-body one'
+      price: [60, 100], class: 'card-body one'
     },
     {
-      active: true, image: 'nutella', flavour: 'Nutella',
+      active: true, image: 'nutella', flavour: 'Delicate Surprise',
       description: `Every Nutella lover's dream in one cookie, topped with the smoothest Nutella frosting & strawberries just to balance it.`,
-      price: [55, 95], class: 'card-body two'
+      price: [90, 130], class: 'card-body two'
     },
     {
-      active: true, image: 'oreos', flavour: 'Oreo Milkshake',
+      active: true, image: 'oreos', flavour: 'Nostalgic Delight ',
       description: `This cookie came to shake your taste buds awake. Two cookies for the price of one. A win-win kind of situation.`,
-      price: [50, 90], class: 'card-body three'
+      price: [75, 115], class: 'card-body three'
     },
-    { active: false, image: 'peanut', flavour: 'Peanut Butter Surprise', description: '', price: [40, 80], class: 'card-body one' },
-    { active: false, image: 'red-velvet', flavour: 'Red Velvet', description: '', price: [45, 85], class: 'card-body two' },
-    // { image: 'coconut', flavour: 'Pina Colada', description: '', price: [40, 80], class: 'card-body one'},
-    // { image: 'rooibos', flavour: 'Rooibos Tea', description: '', price: [40, 80], class: 'card-body two'}
     {
-      active: true, image: 'straw-cream', flavour: 'Strawberries & Cream',
+      active: true, image: 'peanutbutter', flavour: 'Pean-nutty & nice',
+      description: 'What is better than peanut butter? Mmm... yep I thought so, nothing! It will make you go nuts... in a good way.',
+      price: [60, 100], class: 'card-body one'
+    },
+    {
+      active: true, image: 'redvelvet', flavour: 'Queen of Hearts',
+      description: 'Red Velvet cookie, that will leave even the Queen of Hearts jealous, with the smoothest cream cheese frosting.',
+      price: [60, 100], class: 'card-body two'
+    },
+    { active: false, image: 'coconut', flavour: 'Back to the Beach', description: '', price: [75, 115], class: 'card-body one' },
+    { active: false, image: 'rooibos', flavour: 'Mad Hatters Tea Party', description: '', price: [60, 100], class: 'card-body two' },
+    {
+      active: true, image: 'straw-cream', flavour: 'Lovers Paradise',
       description: `We are taking it back to the basics with a twist on a popular dessert. You can have it all in a few simple bites.`,
-      price: [45, 85], class: 'card-body one'
+      price: [60, 100], class: 'card-body three'
     },
     {
-      active: true, image: 'twix', flavour: 'Twix',
+      active: true, image: 'twix', flavour: 'Twixed Dreams',
       description: `Craving something caramel? We think Twix will satisfy that for you in a split second or even sooner...`,
-      price: [50, 90], class: 'card-body two'
+      price: [70, 110], class: 'card-body one'
     },
     {
       active: false, image: 'variety', flavour: 'Variety Box (any 12 flavours)',
       description: ``,
-      price: [0, 100], class: 'card-body two'
+      price: [0, 150], class: ''
     }
   ];
 
@@ -85,6 +93,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.menu = this.cookies;
+
     this.cookies = this.cookies.filter((c: Cookies) => c.active);
   }
 
